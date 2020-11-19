c795eabd1ed7d422c5112bda2c08c8befa06c2b3
Kseniya Konfederatova <52353975+kkonfederatova@users.noreply.github.com>
2020-11-19 16:05:10 +0300

    Update objects.js

--- Обход свойств объекта/objects.js
+++ Обход свойств объекта/objects.js
@@ -6,7 +6,6 @@ import _ from 'lodash';
 // BEGIN (write your solution here)
 export default (data, arr) => {
     const result = {};
-    const keys = Object.keys(data);
     for (const item of arr){
         if (data.hasOwnProperty(item)){
             result.[item] = data.[item];
